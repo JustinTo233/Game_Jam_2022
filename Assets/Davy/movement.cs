4df5e4e8598caffc0cb6431aef6ebfffffba80ee
Teemro <davychi123@gmail.com>
2022-07-15 22:45:39 -0700

    movement implemented

---
+++ Assets/Davy/movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Vertical = Input.GetAxis("Vertical") * 0.01f;
+        float Horizontal = Input.GetAxis("Horizontal") * 0.01f;
+        transform.Translate(0, Vertical, 0);
+        transform.Translate(Horizontal, 0, 0);
+    }
+}
