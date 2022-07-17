39f7f66f5ffefc5b42aa6239441261c0e5aff0ff
Justin To <74964260+jellycandy23@users.noreply.github.com>
2022-07-16 22:47:26 -0700

    Play button works

--- Assets/JustinTo/Scripts/MainMenu.cs
+++ Assets/JustinTo/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayButton()
     {
-        //SceneManager.LoadScene("MainGame");
+        SceneManager.LoadScene("Map");
     }
 
     public void QuitButton()
