d88e37fe0ec09b4c2b9a43cc1cd754a9574b955b
Teemro <davychi123@gmail.com>
2022-07-16 21:22:33 -0700

    Fixed script

--- Assets/Davy/EnemyScript.cs
+++ Assets/Davy/EnemyScript.cs
@@ -13,7 +13,7 @@ public class EnemyScript : MonoBehaviour
     {
         health = maxHealth;
     }
-
+    // take damage
     public void TakeDamage(float damageAmount)
     {
         health -= damageAmount;
@@ -28,7 +28,11 @@ public class EnemyScript : MonoBehaviour
     private void OnCollisionStay2D(Collision2D other) {
         if(other.gameObject.tag == "Player"){
             if(attackSpeed <= canAttack){
-                other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-attackDamage);
+                // other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-attackDamage);
+                if(other.gameObject.CompareTag("Player"))
+                {
+                    other.gameObject.SendMessage("TakeDamage", attackDamage);
+                }
                 canAttack = 0f;
             } else{
                 canAttack += Time.deltaTime;
